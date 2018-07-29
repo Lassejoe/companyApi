a4704cae7a5ec7b48ab93270b32ed848e7d34950
Lassejoe <lasse_johansen12@hotmail.com>
2018-07-29 13:21:01 +0200

    first commitment

---
+++ server.rb
@@ -0,0 +1,136 @@
+# server.rb
+require 'sinatra'
+require "sinatra/namespace"
+require 'mongoid'
+
+# DB Setup
+Mongoid.load! "mongoid.config"
+
+#Data Models
+class Company
+  include Mongoid::Document
+
+  #all the variables that a company can contain
+  field :companyID, type: String
+  field :companyName, type: String
+  field :address, type: String
+  field :city, type: String
+  field :country, type: String
+  field :owners, type: String
+  field :email, type: String
+  field :phoneNumber, type: String
+
+  #validates makes sure that the user provide the correct information, the email and phoneNumber is optional
+  validates :companyID, presence: true
+  validates :companyName, presence: true
+  validates :address, presence: true
+  validates :city, presence: true
+  validates :country, presence: true
+  validates :owners, presence: true
+
+  #assigning a "primary key" I decided to make the companyID unique
+  index({ companyID: 'text'})
+  index({ companyID: 1},{unique: true,drop_dups: true ,name: "companyID_index"})
+
+  #sorting on the different fields
+  scope :companyName, -> (companyName) { where(companyName: /^#{companyName}/i) }
+  scope :companyID, -> (companyID) { where(companyID: /^#{companyID}/) }
+  scope :city, -> (city) { where(city: /^#{city}/i) }
+  scope :country, -> (country) { where(country: /^#{country}/i) }
+  scope :owners, -> (owners) { where(owners: /^#{owners}/i)}
+end
+
+
+#Serializers
+class CompanySerializer
+  def initialize(company)
+    @company = company
+  end
+
+  def as_json(*)
+    data = {
+      id:@company.id.to_s,
+      companyID:@company.companyID,
+      companyName:@company.companyName,
+      address:@company.address,
+      city:@company.city,
+      country:@company.country,
+      owners:@company.owners,
+      email:@company.email,
+      phoneNumber:@company.phoneNumber
+    }
+    data[:errors] = @company.errors if@company.errors.any?
+    data
+  end
+end
+
+
+#api namespace
+namespace '/api/v1' do
+  before do
+    content_type 'application/json'
+  end
+
+  #generates base_url and parse the request body
+  helpers do
+    def base_url
+      @base_url ||= "#{request.env['rack.url_scheme']}://{request.env['HTTP_HOST']}"
+      #found a bug in the @base_url which would return curl(52): Empty reply from server the "" fixed the problem.
+      ""
+    end
+    def json_params
+      begin
+        JSON.parse(request.body.read)
+      rescue
+        halt 400,{message:'Invalid JSON'}.to_json
+      end
+    end
+  end
+
+
+  #call for a specific companyID
+  get '/company/:companyID' do |companyID|
+    company = Company.where(companyID: companyID).first
+    halt(404, {message:'The company you are searching for do not exist'}.to_json) unless company
+    CompanySerializer.new(company).to_json
+  end
+
+  #returns all created companies
+  get '/companies' do
+  companies = Company.all
+  [:companyName, :companyID, :city, :country, :owners].each do |filter|
+    companies = companies.send(filter, params[filter]) if params[filter]
+  end
+  companies.map {|company| CompanySerializer.new(company)}.to_json
+  end
+
+  #create a new company
+  post '/companies' do
+    company = Company.new(json_params)
+    if company.save
+      response.headers['Location'] = "#{base_url}/api/v1/company/#{company.companyID}"
+      status 201
+    else
+      status 422
+      body CompanySerializer.new(company).to_json
+    end
+  end
+  #updates the specific company and the client can update a specific field
+  patch '/company/:companyID' do |companyID|
+    company = Company.whre(companyID: companyID).first
+    halt(404, {message:'The company you are searching for do not exist'}.to_json) unless company
+    if company.update_attributes(json_params)
+      CompanySerializer.new(company).to_json
+    else
+      status 422
+      body CompanySerializer.new(company).to_json
+    end
+  end
+
+  delete '/company/:companyID' do |companyID|
+    company = Company.where(companyID: companyID).first
+    company.destroy if company
+    status 204
+  end
+end
+#Endpoint
